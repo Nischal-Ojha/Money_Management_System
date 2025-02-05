f09e5410e07f9fee96984481c04b9190dd1920c7
Nischal-Ojha <nischalojha59@gmail.com>
2025-02-05 14:40:40 +0545

    Confirmation Added

--- frontend/src/app/api/transactionHistory.ts
+++ frontend/src/app/api/transactionHistory.ts
@@ -5,6 +5,7 @@ export default async function getDatas(location:string){
     try{
         const newData = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${location==="today"?"/api/transactionToday":"/api/transactionHistory"}`)
         const nextdata = await newData.json()
+        console.log("connected to", process.env.NEXT_PUBLIC_API_URL)
         return  await nextdata
       }catch(error){
         console.log(error)
